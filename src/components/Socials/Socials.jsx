b3c483a52cd6b5c0fb8ac744778d7d2a2a909d51
oyesola-dolapo <oyesolabolamide@gmail.com>
2024-10-05 01:47:13 +0100

    socials

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,19 +4,23 @@ export default function Socials() {
   const socials = [
     {
       icon: <i class="fa-brands fa-instagram"></i>,
-      link: "#",
+      link: "https://www.instagram.com/dolapoyesola/",
     },
     {
       icon: <i class="fa-brands fa-linkedin-in"></i>,
-      link: "/rent",
+      link: "https://www.linkedin.com/in/oyesola-pedro-778642173/",
     },
     {
       icon: <i class="fa-brands fa-github"></i>,
-      link: "/rent",
+      link: "https://github.com/oyesola-dolapo",
     },
     {
       icon: <i class="fa-brands fa-x-twitter"></i>,
       link: "https://x.com/ferraziii",
+    },
+    {
+      icon: <i class="fa-brands fa-whatsapp"></i>,
+      link: "https://wa.me/2349067219868",
     },
   ];
   return (
@@ -25,12 +29,15 @@ export default function Socials() {
         Developer by{" "}
         <a
           href="https://www.ferrazi.netlify.app"
+          target="_blank"
           className="font-bold text-primaryColor underline">
           Dolapo Oyesola
         </a>
         &nbsp; Designed by{" "}
         <a
           href="https://www.figma.com/@illiyinstudio"
+          target="_blank"
+          
           className="font-bold text-primaryColor underline">
           Illiyin Studio
         </a>
